f7c8d4c1079c5434e78620a405c04f9bf42caeec
Jeshua <hinostrozajeshua@gmail.com>
2022-08-02 18:58:58 -0500

    route implemented for get notes controller

--- src/v1/note.routes.ts
+++ src/v1/note.routes.ts
@@ -3,6 +3,6 @@ import { getAllNotesByUser } from '../controllers/notes.controllers';
 
 const noteRoutes = Router();
 
-noteRoutes.get('/:id', getAllNotesByUser);
+noteRoutes.get('/', getAllNotesByUser);
 
 export default noteRoutes;
