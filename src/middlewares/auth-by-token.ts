693e8d2ac2b3fbdc6761e7d9cb79297eee25ca55
Jeshua <hinostrozajeshua@gmail.com>
2022-08-03 11:14:50 -0500

    chek if auth tokes is valid

--- src/middlewares/auth-by-token.ts
+++ src/middlewares/auth-by-token.ts
@@ -5,18 +5,25 @@ import { getOneUserById } from '../services/user.services';
 export async function authByToken(request: Request, response: Response, next: NextFunction) {
     const authHeader = request.get('authorization');
 
-    if (!authHeader) return response.status(404).end();
+    if (!authHeader) return response.status(401).json({ error: "token missing" });
 
     const [ auth, token ] = authHeader.split(' ') as [string, string?];
 
     if (auth.toLowerCase() !== 'bearer' || !token) return response.status(401).end();
 
-    const tokenDeco = verify(token, process.env.TOKEN_SECRET!);
+
+    let tokenDeco: any;
+
+    try {
+        tokenDeco = verify(token, process.env.TOKEN_SECRET!);
+    }
+    catch { return response.status(401).json({ error: "token invalid" }); }
+
     if (typeof tokenDeco === 'string' || !tokenDeco.id) return response.status(401).end();
 
     const user = await getOneUserById(tokenDeco.id);
 
-    if (!user) return response.status(404).json({ error: "token invalid" })
+    if (!user) return response.status(404).json({ error: "token invalid" });
 
     response.locals.userAuth = {
         id: tokenDeco.id,
