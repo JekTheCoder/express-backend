113ccd29a5b23830c11f5567d9c713f2897fe689
Jeshua <hinostrozajeshua@gmail.com>
2022-08-02 19:05:37 -0500

    use notes router

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import userRoutes from "./v1/user.routes";
+import noteRoutes from "./v1/note.routes";
 
 const router = Router();
 
-router.use('/v1/users', userRoutes)
+router
+    .use('/v1/users', userRoutes)
+    .use('/v1/notes', noteRoutes);
 
 export default router;
